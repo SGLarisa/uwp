acf8b3a7a37c2f590ccebd3048c60e0b78bf1898
Miltos Katifedenios <miltoskat@hotmail.com>
2017-03-31 00:22:22 +0300

    Modified project
    This commit contains the files of a modified version of the original
    project made to work in real time with the sliders without the need of
    the button.

---
+++ Class-4/Modified/MainPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        byte Red = 0;
+        byte Green = 0;
+        byte Blue = 0;
+        byte Alpha = 255;
+        byte[] rgbhex = { 0, 0, 0 };
+        string hex;
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Red_Slider_Value_Changed(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            Slider slider = sender as Slider;
+            if (slider != null)
+            {
+                Red = Convert.ToByte(slider.Value);
+                ConstListener();
+            }
+        }
+
+        private void Green_Slider_Value_Changed(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            Slider slider = sender as Slider;
+            if (slider != null)
+            {
+                Green = Convert.ToByte(slider.Value);
+                ConstListener();
+            }
+        }
+
+        private void Blue_Slider_Value_Changed(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            Slider slider = sender as Slider;
+            if (slider != null)
+            {
+                Blue = Convert.ToByte(slider.Value);
+                ConstListener();
+            }
+        }
+
+        private void ConstListener()
+        {
+            BGColor.Fill = new SolidColorBrush(Color.FromArgb(Alpha, Red, Green, Blue));
+            if(Red==255 && Green==255 && Blue == 255)
+            {
+                RowOneTextBlock.Foreground = new SolidColorBrush(Colors.Black);
+                RowOneTextBlock.Text = "Too white Mon";
+            }
+            else
+            {
+                RowOneTextBlock.Foreground = new SolidColorBrush(Color.FromArgb(Alpha, Red, Green, Blue));
+                RowOneTextBlock.Text = "Hex Colour Picker";
+            }
+            rgbhex[0] = Red;
+            rgbhex[1] = Green;
+            rgbhex[2] = Blue;
+            hex = BitConverter.ToString(rgbhex).Replace("-", string.Empty);
+            HexColorTextBlock.Text = hex;
+
+        }
+
+        private void Alpha_Slider_Value_Changed(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            Slider slider = sender as Slider;
+            if (slider != null)
+            {
+                Alpha = Convert.ToByte(slider.Value);
+                ConstListener();
+            }
+        }
+    }
+}
---
+++ Class-4/Modified/readme.md
@@ -0,0 +1,19 @@
+# Buttons
+
+## English
+This folder only contains the **XAML** and the **C#** code of the project.
+
+If you want you can test them out by creating a new *project* and using the code in your **MainPage.xaml** and **MainPage.xaml.cs**.
+
+**`ATTENTION DO NOT COPY PASTE THE WHOLE CODE`**  
+**`ALSO PAY ATTENTION TO THE LIBRARIES INCLUDED`**   
+**`THIS FOLDER CONTAINS A MODIFIED VERSION OF THE ORIGINAL PROJECT SHOWN IN CLASSROOM`**
+
+## Ελληνικά
+Ο εξής φάκελος περιέχει μόνο το αρχείο **XAML** και τον κώδικα **C#** του project.
+
+Αν θέλετε να τον δοκιμάσετε, δημιουργείτε ένα καινούργιο *project* στον **Visual Studio** και χρησιμοποιήτε τον κώδικα των αρχείων μέσα στο **MainPage.xaml** και **MainPage.xaml.cs** αντίστοιχα.
+
+**`ΠΡΟΣΟΧΗ ΜΗΝ ΚΑΝΕΤΕ ΑΝΤΙΓΡΑΦΗ ΤΟΝ ΚΩΔΙΚΑ ΑΥΤΟΥΣΙΟ`**  
+**`ΕΠΙΠΛΕΟΝ ΔΩΣΤΕ ΠΡΟΣΟΧΗ ΣΤΙΣ ΒΙΒΛΙΟΘΗΚΕΣ ΠΟΥ ΕΧΟΥΝ ΣΥΜΠΕΡΙΛΙΦΘΕΙ`**  
+**`ΑΥΤΟΣ Ο ΦΑΚΕΛΟΣ ΠΕΡΙΕΧΕΙ ΜΙΑ ΔΙΑΦΟΡΕΤΙΚΗ ΕΚΔΟΣΗ ΤΟΥ ΑΥΘΕΝΤΙΚΟΥ PROJECT ΤΟ ΟΠΟΙΟ ΠΑΡΟΥΣΙΑΣΤΗΚΕ ΣΤΗΝ ΤΑΞΗ`**
